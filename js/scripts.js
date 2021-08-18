70b3044e6b403196b351d21b334824c088a95b3d
Art Weinstein <artur.weinstein@gmail.com>
2021-08-18 15:41:17 -0700

    create a function to address punctuation in a word.
    Co-authored-by: Erica Marroquin <ericamarroquin03@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
+function ifPunctuation(word) {
+  const punctArray = ["!", ".", ",", "/", "?", "-", "(", ")"];
+  for (let i = 0; i < punctArray.length; i++) {
+    if (word.includes(punctArray[i])) {
+      return [true, [i]];
+    } else {
+      return false;
+    }
+  }
+}
+
 function letterVowel(word) {
   let str = word;
   if ( str.charAt(0) === "a" || str.charAt(0) ==="e" || str.charAt(0) === "i" || str.charAt(0) === "i" || str.charAt(0) === "o" || str.charAt(0) === "u") {
@@ -7,12 +18,36 @@ function letterVowel(word) {
   }
 }
 
+function letterConsonant(word){
+  if (letterVowel(word) === false){
+    return true;
+  }  else {
+    return false;
+  }
+}
+
+function quConsonant(word) {
+  if (word.charAt(0) === "q" && word.charAt(1) === "u" && letterVowel(word) === false){
+    return true;
+} else {
+  return false;
+}
+}
+
 function pigLatin(text) {
   const wordArray = text.toLowerCase().split(" ");
   let pigLatinArray = [];
   for (let i = 0; i < wordArray.length; i++) {
     if (letterVowel(wordArray[i])) {
       pigLatinArray.push(wordArray[i] + "way");
+    } else if (quConsonant(wordArray[i])) {
+      const constQU = wordArray[i].charAt(0) + wordArray[i].charAt(1);
+      const wordBody = wordArray[i].slice(2);
+      pigLatinArray.push(wordBody + constQU + "ay");
+    } else if (letterConsonant(wordArray[i])){
+      const firstLetter = wordArray[i].charAt(0);
+      const wordBody = wordArray[i].slice(1);
+      pigLatinArray.push(wordBody + firstLetter + "ay");
     } else {
       pigLatinArray.push(wordArray[i]);
     }
@@ -20,10 +55,4 @@ function pigLatin(text) {
   return pigLatinArray;
 }
 
-function space(sentence) {
-  if (sentence.includes(" ")){
-    return true;
-  } else {
-    return false;
-  }
-}
+if (ifPunction(wordArray[i]))
