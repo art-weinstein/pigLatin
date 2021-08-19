0e124dc9162a30407615126f02d202ae5135da85
Art Weinstein <artur.weinstein@gmail.com>
2021-08-18 18:32:42 -0700

    WIP fix function to focus on moving all punctuation.
    Co-authored-by: Erica Marroquin <ericamarroquin03@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -25,10 +25,11 @@ function quConsonant(word) {
 
 function ifPunctuation(word) {
   const punctArray = ["!", ".", ",", "/", "?", "-", "(", ")"];
-  for (let i = 0; i < punctArray.length; i++) {
+  for (let i = 0; i < punctArray[i].length; i++) {
     if (word.includes(punctArray[i])) {
       const punctType = punctArray[i];
       const indexOfPunct = word.indexOf(punctType);
+      // const removePunct = punctArray[i].slice(indexOfPunct)
       return [true, indexOfPunct];
     } else {
       return false;
@@ -67,7 +68,25 @@ function pigLatin(text) {
     if (letterVowel(wordArray[i]) && ifPunctuation(wordArray[i])) {
       const punctIndex = ifPunctuation(wordArray[i])[1];
       const movePunct = wordArray[i].slice(punctIndex);
-      pigLatinArray.push(wordArray[i] + "way" + movePunct);
+      const removePunct = wordArray[i].slice(0, -1);
+      pigLatinArray.push(removePunct + "way" + movePunct);
+    }  else if (quConsonant(wordArray[i]) && ifPunctuation(wordArray[i])){
+      const punctIndex = ifPunctuation(wordArray[i])[1];
+      const constQU = wordArray[i].charAt(0) + wordArray[i].charAt(1);
+      const wordBody = wordArray[i].slice(2);
+      const movePunct = wordArray[i].slice(punctIndex);
+      const removePunct = wordBody.slice(0, -1);
+      pigLatinArray.push(removePunct + constQU + "ay" + movePunct)
+    } else if (letterConsonant(wordArray[i]) && ifPunctuation(wordArray[i])){
+      const punctIndex = ifPunctuation(wordArray[i])[1];
+      const firstLetter = wordArray[i].charAt(0);
+      const wordBody = wordArray[i].slice(1);
+      const movePunct = wordArray[i].slice(punctIndex);
+      const removePunct = wordBody.slice(0, -1);
+      pigLatinArray.push(removePunct + firstLetter + "ay" + movePunct);
+    } else if (letterVowel(wordArray[i])){
+
+      pigLatinArray.push(wordArray[i] + "way");
     } else if (quConsonant(wordArray[i])) {
       const constQU = wordArray[i].charAt(0) + wordArray[i].charAt(1);
       const wordBody = wordArray[i].slice(2);
